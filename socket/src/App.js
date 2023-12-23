6fddaee8f7276752db542d77275de6f3929abc35
blee94 <135qudwls@naver.com>
2023-12-23 15:39:59 +0900

    socket

--- socket/src/App.js
+++ socket/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Chatting1 from './components/Chatting';
+import Chatting3 from './components/Chatting3';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Chatting1 />
+      <Chatting3 />
+    </div>
+  );
 }
 
 export default App;
